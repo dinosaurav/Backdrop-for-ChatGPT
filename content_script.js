e0c59f1fde6b38268bd60f6ec5e8d404e8afbcef
Saurav Das <sauravd@gmail.com>
2023-04-15 17:29:58 -0700

    adds content script

---
+++ content_script.js
@@ -0,0 +1,18 @@
+// get page details
+
+// LATER get access details
+
+// get page text
+
+// NVM get page html
+
+// LATER get screenshot
+
+// get all and send to server
+
+
+// server will have 5 methods
+// accept info
+// get details
+// get page text
+// LATER get screenshot
