161fa31fa8cdeb66b35b0192e309d6cfd72c3844
Saurav Das <sauravd@gmail.com>
2023-04-16 03:15:48 -0700

    working commit

--- content_script.js
+++ content_script.js
@@ -1,18 +1,39 @@
-// get page details
+function getTitle(){
+    return document.title;
+}
+function getText(){
+    return document.body.innerText;
+}
+function uploadText() {
+    appurl = "https://nixqjb-8000.csb.app/upload-text/";
+    payload = JSON.stringify({
+        "title": title,
+        "text": text
+    });
+    console.log(payload);
 
-// LATER get access details
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", appurl);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
 
-// get page text
+    xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+    }};
+    xhr.send(payload);
+    console.log("sent to " + appurl + " payload: " + payload);
+}
+console.log(getText());             //Gives you all the text on the page
+console.log("DINOSAURAV5");
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("text requested");
+    title = getTitle();
+    text = getText();
 
-// NVM get page html
-
-// LATER get screenshot
-
-// get all and send to server
-
-
-// server will have 5 methods
-// accept info
-// get details
-// get page text
-// LATER get screenshot
+    if (request.action === "getText") {
+      sendResponse(getText());
+    }
+    uploadText(title, text);
+  });
--- popup.js
+++ popup.js
@@ -1,3 +1,9 @@
-//Javascript here
-
-
+var query = { active: true, currentWindow: true };
+function callback(tabs) {
+    var currentTab = tabs[0]; // there will be only one in this array
+    console.log(currentTab); // also has properties like currentTab.id
+    chrome.tabs.sendMessage(currentTab.id, { action: "getText" }, (response) => {
+        console.log("SDF");
+    });
+}
+chrome.tabs.query(query, callback);
